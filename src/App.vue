f4a60515af70170d17742cb103326ee26b499d22
Khaled Faysal <khaled.faysal@deepki.com>
2024-03-25 17:13:43 +0100

    repo for example

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <section>
+    <header>
+      {{
+        $formatMessage({
+          defaultMessage: 'Control Panel',
+          description: 'title of control panel section',
+        })
+      }}
+    </header>
+    <ul>
+      <li>
+        <button>
+          {{
+            $formatMessage(
+              {
+                defaultMessage: 'Delete user {name}',
+                description: 'Delete button',
+              },
+              {
+                name: props.name,
+              }
+            )
+          }}
+        </button>
+      </li>
+      <li>
+        <input :placeholder="newPwdPlaceholder" />
+        <input :placeholder="confirmPwdPlaceholder" />
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  inject: ['intl'],
+  props: ['name'],
+  data() {
+    return {
+      newPwdPlaceholder: $formatMessage({
+        defaultMessage: 'New Password',
+        description: 'placeholder text',
+      }),
+      confirmPwdPlaceholder: $formatMessage({
+        id: 'explicit-id',
+        defaultMessage: 'Confirm Password',
+        description: 'placeholder text',
+      }),
+    }
+  },
+}
+</script>
